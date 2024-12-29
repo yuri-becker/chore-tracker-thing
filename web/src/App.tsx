af594399fd023af03edbd6bc6936d94284969911
miyas_lappup <miya@fastmail.org>
2024-12-29 02:08:04 +0100

    feat: add burger menu

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,53 @@
-import { Button } from '@ariakit/react'
 import { useUser } from './use-user.tsx'
 import '@picocss/pico/css/pico.css'
 import HouseholdForm from './household-form.tsx'
+import { Button } from '@ariakit/react'
+import { useState } from 'react'
+import { Menu, LogOut, Settings, Plus, X } from 'react-feather'
 
 function App () {
   const user = useUser()
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
 
   return (
     <>
-      {!user && <a href="/oidc/login">
-        <Button>Login</Button>
-      </a>}
-      {user && <>
-        <a href="/oidc/logout">
-          <Button>Logout {user.name}</Button>
+      {!user && <>
+        <a href="/oidc/login">
+          <Button>Login</Button>
         </a>
-        <HouseholdForm/>
+      </>}
+
+      {user && <>
+        <aside className={isMenuOpen ? 'open main-menu' : 'main-menu'}>
+          <div>
+            <Button className={'outline secondary flat'} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+              <X/>
+            </Button>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <Plus/>&nbsp;Create Task
+              </li>
+              <li>
+                <Settings/>&nbsp;Settings
+              </li>
+              <li>
+                <a className="contrast" href="/oidc/logout"><LogOut/>&nbsp;Logout {user.name}</a>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+        <main className="container">
+          <header>
+            <Button id="burger-menu" className={'outline secondary flat'} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+              <Menu/>
+            </Button>
+          </header>
+          <HouseholdForm/>
+        </main>
       </>
       }
-
     </>
   )
 }
