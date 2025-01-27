719a327c60c010d03b26ffb8dcd20399dedfe64a
pubbi <miya@fastmail.org>
2025-01-27 19:10:02 +0100

    Fix linter error

--- web/src/global/household-context.provider.tsx
+++ web/src/global/household-context.provider.tsx
@@ -5,6 +5,7 @@ import { HouseholdContext } from './household-contextt.tsx'
 export const HouseholdContextProvider = ({ children }: { children: ReactNode }) => {
   const [households, setHouseholds] = useState<household[]>()
   useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
     fetch('/api/household').then(res => res.json()).then(households => setHouseholds(households as household[])).catch()
   }, [])
   return <HouseholdContext.Provider value={households}>
